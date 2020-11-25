88a7dc3738f7396c9456cf17fe6651bda7c68b4f
Derek Chen <derekchen72@gmail.com>
2020-11-25 16:12:36 -0500

    finished association exercise

---
+++ Associations Exercise/skeleton/app/models/enrollment.rb
@@ -0,0 +1,11 @@
+class Enrollment < ApplicationRecord
+    belongs_to :course,
+    class_name: "Course",
+    foreign_key: :course_id,
+    primary_key: :id
+
+    belongs_to :user,
+    class_name: "User",
+    foreign_key: :student_id,
+    primary_key: :id
+end
